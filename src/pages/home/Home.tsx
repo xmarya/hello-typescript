9832f10d3199e1c7b2e8bde6bd3b44c9c70f1791
xmarya <marya_hy@hotmail.com>
2024-11-27 18:36:11 +0300

    started in the main content part

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,14 @@
-import './home.scss';
+import "./home.scss";
 
 export default function Home() {
-    return (
-        <div className="home-page">
-            Home page
-        </div>
-    )
+  return (
+    <div className="home-page">
+      <div className="top-deals">top deals</div>
+      <div className="visits">visits</div>
+      <div className="totals">totals</div>
+      <div className="analytics">analytics</div>
+      <div className="lead-by">lead-by</div>
+      <div className="profit">profit</div>
+    </div>
+  );
 }
-

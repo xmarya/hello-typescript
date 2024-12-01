062ab6c97881d8209f9dbd89e7abb3158c9c720c
xmarya <marya_hy@hotmail.com>
2024-12-01 09:52:15 +0300

    finished the home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,7 @@
+import AreaChartBox from "../../components/AreaChartBox";
 import BarChartBox from "../../components/BarChartBox";
 import ChartBox from "../../components/ChartBox";
+import PieChartBox from "../../components/PieChartBox";
 import TopBox from "../../components/TopBox";
 import { barChartBoxRevenue, barChartBoxVisits, chartBoxConversion, chartBoxProduct, chartBoxRevenue, chartBoxUser } from "../../data/charts";
 import "./home.scss";
@@ -8,17 +10,26 @@ export default function Home() {
   return (
     <div className="home-page">
       <div className="top-deals"><TopBox/></div>
+
       <div className="visits">
         <BarChartBox<"visits"> data={barChartBoxVisits} />
       </div>
+
       <div className="totals">
         <ChartBox<"users"> data={chartBoxUser}/>
         <ChartBox<"products"> data={chartBoxProduct}/>
         <ChartBox<"revenue"> data={chartBoxRevenue}/>
         <ChartBox<"ratio"> data={chartBoxConversion}/>
       </div>
-      <div className="analytics">analytics</div>
-      <div className="lead-by">lead-by</div>
+
+      <div className="analytics">
+        <AreaChartBox/>
+      </div>
+
+      <div className="lead-by">
+        <PieChartBox/>
+      </div>
+
       <div className="profit">
         <BarChartBox<"profit"> data={barChartBoxRevenue}/>
       </div>
